9f7a74167632150abfc3f65584b8e159d2458fb3
orbit0ver <orbit0ver@>
2015-10-30 02:15:58 +0900

    計算ミスの修正

--- my_tracking.c
+++ my_tracking.c
@@ -155,13 +155,13 @@ void setXY(const unsigned char *image, bounding_box *obox, int width, int height
 	if ( preX1 > preX2 ) {
 		i0 = x1 + minX;
 	} else if ( preX1 < preX2 ){
-		i0 = x2 + minX - box.w;
+		i0 = x2 + minX - (box.w - 1);
 	}
 
 	if (  preY1 > preY2) {
 		j0 = y1 + minY;
 	} else if ( preY1 < preY2 ) {
-		j0 = y2 + minY - box.h;
+		j0 = y2 + minY - (box.h - 1);
 	}
 
 	obox->x = i0;
