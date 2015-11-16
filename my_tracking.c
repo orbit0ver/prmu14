dce86fdee397597710abae06e3f653be68476c38
tamu <tamu@mailmail>
2015-11-16 17:03:57 +0900

    エッジ検出する際の範囲を変更、リファクタリング

--- my_tracking.c
+++ my_tracking.c
@@ -6,6 +6,7 @@
 
 #define SEARCH_RANGE 50	/* 移動物体の探索範囲 */
 #define MISSRANGE 10		/* 探索ミスカウントの上限 */
+#define MERR 1000
 
 #define PIX(img, x, y, w, i) ( img[( (x) + (y) * (w) ) * 3 + (i)] )
 #define MINTEST(val, min) ( ((val) < (min)) ? (min) : (val) )
@@ -20,6 +21,7 @@ typedef struct {
 
 bounding_box obox;		/* 前フレームにおける移動物体を囲む矩形 */
 unsigned char *_preImg = NULL;	/* 前フレームの探索範囲の画像 */
+bounding_box _searchBox;
 
 
 double calcSSD(const unsigned char *srcImg, const unsigned char *destImg, int x, int y, int width);
@@ -236,26 +238,35 @@ void my_tracking_level1( int frameID, unsigned char *image, int width, int heigh
 
 
 
-bounding_box setSearchBox(const bounding_box box, int width, int height)
+
+
+/*
+
+ start level 2
+
+ */
+
+
+void setSearchBox(const bounding_box box, int width, int height)
 {
-	bounding_box searchBox = box;
-	searchBox.x -= SEARCH_RANGE;
-	searchBox.y -= SEARCH_RANGE;
-	searchBox.w += SEARCH_RANGE * 2;
-	searchBox.h += SEARCH_RANGE * 2;
+	// 探索範囲の初期値設定
+	_searchBox = box;
+	_searchBox.x -= SEARCH_RANGE;
+	_searchBox.y -= SEARCH_RANGE;
+	_searchBox.w += SEARCH_RANGE * 2;
+	_searchBox.h += SEARCH_RANGE * 2;
 
-	searchBox.x = MINTEST(searchBox.x, 0);
-	searchBox.y = MINTEST(searchBox.y, 0);
+	// 探索範囲の上限下限の設定
+	_searchBox.x = MINTEST(_searchBox.x, 0);
+	_searchBox.y = MINTEST(_searchBox.y, 0);
 
-	if ( (searchBox.x + searchBox.w) > width  ) {
-		searchBox.w -= (searchBox.x + searchBox.w) - width;
+	if ( (_searchBox.x + _searchBox.w) > width  ) {
+		_searchBox.w -= (_searchBox.x + _searchBox.w) - width;
 	}
 
-	if ( (searchBox.y + searchBox.h) > height  ) {
-		searchBox.h -= (searchBox.y + searchBox.h) - height;
+	if ( (_searchBox.y + _searchBox.h) > height  ) {
+		_searchBox.h -= (_searchBox.y + _searchBox.h) - height;
 	}
-
-	return searchBox;
 }
 
 void setPreImg(const unsigned char *srcImg, bounding_box box, int width, int height)
@@ -263,16 +274,20 @@ void setPreImg(const unsigned char *srcImg, bounding_box box, int width, int hei
 	int x, y;
 	int maxX, maxY;
 
-	// 探索範囲の領域確保
+	// 画像の領域確保
 	if ( _preImg == NULL )  {
-		_preImg = (unsigned char *)malloc( box.w * box.h * 3 * sizeof(unsigned char) );
+		_preImg = (unsigned char *)malloc( width * height * 3 * sizeof(unsigned char) );
 	}
 
-	/* 探索範囲の画像確保 */
+	// 探索範囲の値取得
 	maxX = box.x + box.w;
 	maxY = box.y + box.h;
 	for ( y = box.y; y < maxY; y++ ) {
+		if ( y < 0 || height <= y ) continue;
+
 		for ( x = box.x; x < maxX; x++ ) {
+			if ( x < 0 || width <= x ) continue;
+
 			PIX(_preImg, x, y, width, 0) = PIX(srcImg, x, y, width, 0);
 			PIX(_preImg, x, y, width, 1) = PIX(srcImg, x, y, width, 1);
 			PIX(_preImg, x, y, width, 2) = PIX(srcImg, x, y, width, 2);
@@ -280,20 +295,20 @@ void setPreImg(const unsigned char *srcImg, bounding_box box, int width, int hei
 	}
 }
 
-BoundingXY searchOutside(const unsigned char *image, bounding_box box, BoundingXY boxXY, int width)
+BoundingXY searchOutside(const unsigned char *image, BoundingXY boxXY, int width)
 {
 	double err, merr;
 	int m, n;
 	int missCount;
 	int x1, x2, y1, y2;
 
-	merr = 1000;
+	merr = MERR;
 	getBoundingXY(boxXY, &x1, &x2, &y1, &y2);
 
 	// 上方向
 	missCount = 0;
-	for ( n = y1; n >= box.y; n-- ) {
-		for ( m = box.x; m < box.x + box.w; m++ ) {
+	for ( n = y1; n >= _searchBox.y; n-- ) {
+		for ( m = _searchBox.x; m < _searchBox.x + _searchBox.w; m++ ) {
 			err = calcSSD(_preImg, image, m, n, width);
 			if ( err > merr ) {
 				y1 = n;
@@ -307,8 +322,8 @@ BoundingXY searchOutside(const unsigned char *image, bounding_box box, BoundingX
 
 	// 下方向
 	missCount = 0;
-	for ( n = y2; n < box.y + box.h; n++ ) {
-		for ( m = box.x; m < box.x + box.w; m++ ) {
+	for ( n = y2; n < _searchBox.y + _searchBox.h; n++ ) {
+		for ( m = _searchBox.x; m < _searchBox.x + _searchBox.w; m++ ) {
 			err = calcSSD(_preImg, image, m, n, width);
 			if ( err > merr ) {
 				y2 = n;
@@ -322,7 +337,7 @@ BoundingXY searchOutside(const unsigned char *image, bounding_box box, BoundingX
 
 	// 左方向
 	missCount = 0;
-	for ( m = x1; m >= box.x; m-- ) {
+	for ( m = x1; m >= _searchBox.x; m-- ) {
 		for ( n = y1; n <= y2; n++ ) {
 			err = calcSSD(_preImg, image, m, n, width);
 			if ( err > merr ) {
@@ -337,7 +352,7 @@ BoundingXY searchOutside(const unsigned char *image, bounding_box box, BoundingX
 
 	// 右方向
 	missCount = 0;
-	for ( m = x2; m < box.x + box.w; m++ ) {
+	for ( m = x2; m < _searchBox.x + _searchBox.w; m++ ) {
 		for ( n = y1; n <= y2; n++ ) {
 			err = calcSSD(_preImg, image, m, n, width);
 			if ( err > merr ) {
@@ -355,14 +370,15 @@ BoundingXY searchOutside(const unsigned char *image, bounding_box box, BoundingX
 	return boxXY;
 }
 
-BoundingXY searchInside(const unsigned char *image, bounding_box box, BoundingXY boxXY, int width)
+BoundingXY searchInside(const unsigned char *image, BoundingXY boxXY, int width)
 {
 	double err, merr;
 	int m, n;
 	int missCount;
 	int x1, x2, y1, y2;
+	double amp;
 
-	merr = 1000;
+	merr = MERR;
 	getBoundingXY(boxXY, &x1, &x2, &y1, &y2);
 
 	// 下方向
@@ -371,9 +387,12 @@ BoundingXY searchInside(const unsigned char *image, bounding_box box, BoundingXY
 		for ( m = x1; m <= x2; m++ ) {
 			err = calcSSD(_preImg, image, m, n, width);
 			if ( err > merr ) {
-				y1 = n;
-				missCount = -1;
-				break;
+				amp = sobelFilter(image, m, n, width);
+				if ( amp >= 80 ) {
+					y1 = n;
+					missCount = -1;
+					break;
+				}
 			}
 		}
 		if ( missCount < 0 ) break;
@@ -385,9 +404,12 @@ BoundingXY searchInside(const unsigned char *image, bounding_box box, BoundingXY
 		for ( m = x1; m <= x2; m++ ) {
 			err = calcSSD(_preImg, image, m, n, width);
 			if ( err > merr ) {
-				y2 = n;
-				missCount = -1;
-				break;
+				amp = sobelFilter(image, m, n, width);
+				if ( amp >= 80 ) {
+					y2 = n;
+					missCount = -1;
+					break;
+				}
 			}
 		}
 		if ( missCount < 0) break;
@@ -399,9 +421,12 @@ BoundingXY searchInside(const unsigned char *image, bounding_box box, BoundingXY
 		for ( n = y1; n <= y2; n++ ) {
 			err = calcSSD(_preImg, image, m, n, width);
 			if ( err > merr ) {
-				x1 = m;
-				missCount = -1;
-				break;
+				amp = sobelFilter(image, m, n, width);
+				if ( amp >= 80 ) {
+					x1 = m;
+					missCount = -1;
+					break;
+				}
 			}
 		}
 		if ( missCount < 0 ) break;
@@ -413,83 +438,12 @@ BoundingXY searchInside(const unsigned char *image, bounding_box box, BoundingXY
 		for ( n = y1; n <= y2; n++ ) {
 			err = calcSSD(_preImg, image, m, n, width);
 			if ( err > merr ) {
-				x2 = m;
-				missCount = -1;
-				break;
-			}
-		}
-		if ( missCount < 0 ) break;
-	}
-
-	setBoundingXY(&boxXY, x1, x2, y1, y2);
-
-	return boxXY;
-}
-
-BoundingXY checkEdgeInside(const unsigned char *image, bounding_box box, BoundingXY boxXY, int width)
-{
-	int m, n;
-	int missCount;
-	int x1, x2, y1, y2;
-	double amp;
-
-	getBoundingXY(boxXY, &x1, &x2, &y1, &y2);
-
-	// 右方向
-	missCount = 0;
-	for ( m = x1; m <= x2; m++ ) {
-		for ( n = y1; n <= y2; n++ ) {
-			amp = sobelFilter(image, m, n, width);
-			if ( amp >= 80 ) {
-				// fprintf(stderr, "%s\n", "aaaaaaa");
-				x1 = m;
-				missCount = -1;
-				break;
-			}
-		}
-		if ( missCount < 0 ) break;
-	}
-
-	// 左方向
-	missCount = 0;
-	for ( m = x2; m >= x1; m-- ) {
-		for ( n = y1; n <= y2; n++ ) {
-			amp = sobelFilter(image, m, n, width);
-			if ( amp >= 80 ) {
-				// fprintf(stderr, "%s\n", "aaaaaaa");
-				x2 = m;
-				missCount = -1;
-				break;
-			}
-		}
-		if ( missCount < 0 ) break;
-	}
-
-	// 上方向
-	missCount = 0;
-	for ( n = y1; n <= y2; n++ ) {
-		for ( m = x1; m <= x2; m++ ) {
-			amp = sobelFilter(image, m, n, width);
-			if ( amp >= 80 ) {
-				// fprintf(stderr, "%s\n", "aaaaaaa");
-				y1 = n;
-				missCount = -1;
-				break;
-			}
-		}
-		if ( missCount < 0 ) break;
-	}
-
-	// 下方向
-	missCount = 0;
-	for ( n = y2; n >= y1; n-- ) {
-		for ( m = x1; m <= x2; m++ ) {
-			amp = sobelFilter(image, m, n, width);
-			if ( amp >= 80 ) {
-				// fprintf(stderr, "%s\n", "aaaaaaa");
-				y2 = n;
-				missCount = -1;
-				break;
+				amp = sobelFilter(image, m, n, width);
+				if ( amp >= 80 ) {
+					x2 = m;
+					missCount = -1;
+					break;
+				}
 			}
 		}
 		if ( missCount < 0 ) break;
@@ -503,11 +457,9 @@ BoundingXY checkEdgeInside(const unsigned char *image, bounding_box box, Boundin
 void setXY2(const unsigned char *image, bounding_box *obox, int width, int height, int id)
 {
 	bounding_box box;
-	bounding_box searchBox;
 	BoundingXY boxXY;
 
 	box = *obox;
-	searchBox = setSearchBox(box, width, height);
 
 	boxXY.minX = box.x;
 	boxXY.maxX = box.x + box.w - 1;
@@ -517,13 +469,10 @@ void setXY2(const unsigned char *image, bounding_box *obox, int width, int heigh
 	/* 前フレームと現フレームの探索範囲での変化を調べ、移動物体位置を探索
 	上下左右それぞれの方向に走査していく
 	外側に向けて変化している位置を特定 */
-	boxXY = searchOutside(image, searchBox, boxXY, width);
+	boxXY = searchOutside(image,  boxXY, width);
 
-	// 内側に向けて変化している位置を特定
-	boxXY = searchInside(image, searchBox, boxXY, width);
-
-	// 内側に向けてエッジ検出を行う
-	boxXY = checkEdgeInside(image, searchBox, boxXY, width);
+	// 内側に向けて変化している位置を特定しエッジ検出を行う
+	boxXY = searchInside(image, boxXY, width);
 
 	obox->x = boxXY.minX;
 	obox->y = boxXY.minY;
@@ -534,15 +483,8 @@ void setXY2(const unsigned char *image, bounding_box *obox, int width, int heigh
 /* レベル2用 */
 void my_tracking_level2( int frameID, unsigned char *image, int width, int height )
 {
-	bounding_box searchBox;
-	if( frameID == 0 )
-	{
-		/* 最初のフレームは追跡開始位置を記憶 */
-		// 001,540,132,86,80
-		// 002,5,230,202,121
-		// 003,415,213,215,121
-		// 004,505,230,26,56
-		// 005,157,256,53,48
+	if ( frameID == 0 ) {
+		// 移動物体を検出するため全領域を設定
 		obox.x = 0;
 		obox.y = 0;
 		obox.w = width;
@@ -551,10 +493,9 @@ void my_tracking_level2( int frameID, unsigned char *image, int width, int heigh
 		/* 移動物体を囲む矩形を設定 */
 		set_result( frameID, obox );
 
-		setPreImg(image, obox, width, height);
-	}
-	else
-	{
+		setSearchBox(obox, width, height);
+		setPreImg(image, _searchBox, width, height);
+	} else {
 		/* 前回の物体位置を基準にして現在の物体位置を探索 */
 		setXY2(image, &obox, width, height, frameID);
 		// printf("%d %d\n",obox.x, obox.y );
@@ -562,15 +503,16 @@ void my_tracking_level2( int frameID, unsigned char *image, int width, int heigh
 		/* 移動物体を囲む矩形を保存 */
 		set_result( frameID, obox );
 
-		searchBox = setSearchBox(obox, width, height);
+		setSearchBox(obox, width, height);
 		/* 現在フレームの探索範囲を確保 */
-		setPreImg(image, searchBox, width, height);
+		setPreImg(image, _searchBox, width, height);
 	}
 }
 
 /* レベル3用 */
 void my_tracking_level3( int frameID, unsigned char *image, int width, int height )
 {
+	my_tracking_level2(frameID, image, width, height);
 }
 
 
