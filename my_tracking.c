4828d81adffd61f27251c388349df6f79ecbfb0a
tamu <tamu@localhost.localdomain>
2016-01-08 17:22:31 +0900

    前フレーム検出位置にも物体検出を行うを削除 リファクタリング

--- my_tracking.c
+++ my_tracking.c
@@ -23,186 +23,18 @@ typedef struct {
 bounding_box obox;		/* 前フレームにおける移動物体を囲む矩形 */
 unsigned char *_preImg = NULL;	/* 前フレームの探索範囲の画像 */
 bounding_box _searchBox;
-int _prePos[4][2];
 
 
 double calcSSD(const unsigned char *srcImg, const unsigned char *destImg, int x, int y, int width);
 void getBoundingXY(BoundingXY box, int *x1, int *x2, int *y1, int *y2);
 void setBoundingXY(BoundingXY *box, int x1, int x2, int y1, int y2);
 double sobelFilter(const unsigned char *image, int x, int y, int width, int height);
-
-
-
-void getImage(const unsigned char *srcImg, const bounding_box box, int width, int height)
-{
-	int minX, minY;
-	int destWidth, destHeight;
-	int x, y;
-
-	minX = box.x - SEARCH_RANGE;
-	minY = box.y - SEARCH_RANGE;
-	destWidth = box.w + SEARCH_RANGE * 2;
-	destHeight = box.h + SEARCH_RANGE * 2;
-
-	if ( _preImg == NULL ) {
-		_preImg = (unsigned char *)malloc( destWidth * destHeight * 3 * sizeof(unsigned char) );
-	}
-
-	/* 探索範囲の画像確保 */
-	for ( y = 0; y < destHeight; y++ ) {
-		if ( y + minY >= height || y + minY < 0 ) continue;
-
-		for ( x = 0; x < destWidth; x++ ) {
-			if ( x + minX >= width || x + minX < 0 ) continue;
-
-			PIX(_preImg, x, y, destWidth, 0) = PIX(srcImg, x + minX, y + minY, width, 0);
-			PIX(_preImg, x, y, destWidth, 1) = PIX(srcImg, x + minX, y + minY, width, 1);
-			PIX(_preImg, x, y, destWidth, 2) = PIX(srcImg, x + minX, y + minY, width, 2);
-		}
-	}
-}
-
-void setXY(const unsigned char *image, bounding_box *obox, int width, int height)
-{
-	int e, err, merr;
-	int minX, minY;
-	int serachWidth, serachHeight;
-	int i0, j0;
-	int preX1, preX2, preY1, preY2;
-	int x1, x2, y1, y2; // 変化した位置のx方向y方向の最小位置（1）と最大位置（2）を格納
-	int m, n;
-	int missCount;
-	bounding_box box;
-	box = *obox;
-
-	i0 = box.x;
-	j0 = box.y;
-	merr = 1000;
-
-	minX = box.x - SEARCH_RANGE;
-	minY = box.y - SEARCH_RANGE;
-	serachWidth = box.w + SEARCH_RANGE * 2;
-	serachHeight = box.h + SEARCH_RANGE * 2;
-
-	x1 = preX1 = SEARCH_RANGE;
-	x2 = preX2 = preX1 + box.w;
-	y1 = preY1 = SEARCH_RANGE;
-	y2 = preY2 = preY1 + box.h;
-
-	/* 前フレームと現フレームの探索範囲での変化を調べ、移動物体位置を探索
-	上下左右それぞれの方向に走査していく */
-
-	// 上方向
-	missCount = 0;
-	for ( n = y1; n >= 0; n-- ) {
-		if ( n + minY >= height || n + minY < 0 ) continue;
-
-		for ( m = 0; m < serachWidth; m++ ) {
-			if ( m + minX >= width || m + minX < 0 ) continue;
-			err = 0;
-
-			e = PIX(_preImg, m, n, serachWidth, 0) - PIX(image, m + minX, n + minY, width, 0); err += e * e;
-			e = PIX(_preImg, m, n, serachWidth, 1) - PIX(image, m + minX, n + minY, width, 1); err += e * e;
-			e = PIX(_preImg, m, n, serachWidth, 2) - PIX(image, m + minX, n + minY, width, 2); err += e * e;
-			if ( err > merr ) {
-				y1 = n;
-				missCount = 0;
-				break;
-			}
-		}
-		missCount++;
-		if ( missCount >= MISSRANGE) break;
-	}
-
-	// 下方向
-	missCount = 0;
-	for ( n = y2; n < serachHeight; n++ ) {
-		if ( n + minY >= height || n + minY < 0 ) continue;
-
-		for ( m = 0; m < serachWidth; m++ ) {
-			if ( m + minX >= width || m + minX < 0 ) continue;
-			err = 0;
-
-			e = PIX(_preImg, m, n, serachWidth, 0) - PIX(image, m + minX, n + minY, width, 0); err += e * e;
-			e = PIX(_preImg, m, n, serachWidth, 1) - PIX(image, m + minX, n + minY, width, 1); err += e * e;
-			e = PIX(_preImg, m, n, serachWidth, 2) - PIX(image, m + minX, n + minY, width, 2); err += e * e;
-			if ( err > merr ) {
-				y2 = n;
-				missCount = 0;
-				break;
-			}
-		}
-		missCount++;
-		if ( missCount >= MISSRANGE ) break;
-	}
-
-	// 左方向
-	missCount = 0;
-	for ( m = x1; m >= 0; m-- ) {
-		if ( m + minX >= width || m + minX < 0 ) continue;
-
-		for ( n = y1; n <= y2; n++ ) {
-			if ( n + minY >= height || n + minY < 0 ) continue;
-			err = 0;
-
-			e = PIX(_preImg, m, n, serachWidth, 0) - PIX(image, m + minX, n + minY, width, 0); err += e * e;
-			e = PIX(_preImg, m, n, serachWidth, 1) - PIX(image, m + minX, n + minY, width, 1); err += e * e;
-			e = PIX(_preImg, m, n, serachWidth, 2) - PIX(image, m + minX, n + minY, width, 2); err += e * e;
-			if ( err > merr ) {
-				x1 = m;
-				missCount = 0;
-				break;
-			}
-		}
-		missCount++;
-		if ( missCount >= MISSRANGE ) break;
-	}
-
-	// 右方向
-	missCount = 0;
-	for ( m = x2; m < serachWidth; m++ ) {
-		if ( m + minX >= width || m + minX < 0 ) continue;
-
-		for ( n = y1; n <= y2; n++ ) {
-			if ( n + minY >= height || n + minY < 0 ) continue;
-			err = 0;
-
-			e = PIX(_preImg, m, n, serachWidth, 0) - PIX(image, m + minX, n + minY, width, 0); err += e * e;
-			e = PIX(_preImg, m, n, serachWidth, 1) - PIX(image, m + minX, n + minY, width, 1); err += e * e;
-			e = PIX(_preImg, m, n, serachWidth, 2) - PIX(image, m + minX, n + minY, width, 2); err += e * e;
-			if ( err > merr ) {
-				x2 = m;
-				missCount = 0;
-				break;
-			}
-		}
-		missCount++;
-		if ( missCount >= MISSRANGE ) break;
-	}
-
-	// 移動物体の幅や高さと変化したそれぞれの位置をもとに移動物体の位置を特定
-	// x方向とy方向それぞれ変化の大きい方の計算を行う
-	preX1 -= x1;
-	preX2 = x2 - preX2;
-	preY1 -= y1;
-	preY2 = y2 - preY2;
-
-	if ( preX1 > preX2 ) {
-		i0 = x1 + minX;
-	} else if ( preX1 < preX2 ){
-		i0 = x2 + minX - box.w;
-	}
-
-	if (  preY1 > preY2) {
-		j0 = y1 + minY;
-	} else if ( preY1 < preY2 ) {
-		j0 = y2 + minY - box.h;
-	}
-
-	obox->x = i0;
-	obox->y = j0;
-	// printf("y1:%d y2:%d\n", y1, y2);
-}
+void setSearchBox(const bounding_box box, int width, int height);
+void setPreImg(const unsigned char *srcImg, bounding_box box, int width, int height);
+BoundingXY searchOutside(const unsigned char *image, BoundingXY boxXY, int width);
+void setXY(const unsigned char *image, bounding_box *obox, int width, int height);
+BoundingXY searchInside(const unsigned char *image, BoundingXY boxXY, int width, int height);
+void setXY2(const unsigned char *image, bounding_box *obox, int width, int height, int id);
 
 
 
@@ -214,43 +46,117 @@ void my_tracking_level1( int frameID, unsigned char *image, int width, int heigh
 		save_bmp( "hoge.bmp", image, width, height );
 	*/
 
-	if( frameID == 0 )
-	{
+	if( frameID == 0 ) {
 		/* 最初のフレームは追跡開始位置を記憶 */
 		obox = *box0;
+		set_result( frameID, obox );
+
+		setSearchBox(obox, width, height);
+		setPreImg(image, _searchBox, width, height);
+	} else {
+		/* 前回の物体位置を基準にして現在の物体位置を探索 */
+		setXY(image, &obox, width, height);
+
+		/* 移動物体を囲む矩形を保存 */
+		set_result( frameID, obox );
+
+		setSearchBox(obox, width, height);
+		setPreImg(image, _searchBox, width, height);
+	}
+}
+
+/* レベル2用 */
+void my_tracking_level2( int frameID, unsigned char *image, int width, int height )
+{
+	if ( frameID == 0 ) {
+		// 移動物体を検出するため全領域を設定
+		obox.x = 0;
+		obox.y = 0;
+		obox.w = width;
+		obox.h = height;
 
 		/* 移動物体を囲む矩形を設定 */
 		set_result( frameID, obox );
 
-		getImage(image, obox, width, height);
-	}
-	else
-	{
+		setSearchBox(obox, width, height);
+		setPreImg(image, _searchBox, width, height);
+	} else {
 		/* 前回の物体位置を基準にして現在の物体位置を探索 */
-		setXY(image, &obox, width, height);
+		setXY2(image, &obox, width, height, frameID);
 		// printf("%d %d\n",obox.x, obox.y );
 
 		/* 移動物体を囲む矩形を保存 */
 		set_result( frameID, obox );
 
+		setSearchBox(obox, width, height);
 		/* 現在フレームの探索範囲を確保 */
-		getImage(image, obox, width, height);
+		setPreImg(image, _searchBox, width, height);
 	}
+}
+
+/* レベル3用 */
+void my_tracking_level3( int frameID, unsigned char *image, int width, int height )
+{
+	my_tracking_level2(frameID, image, width, height);
 }
 
 
 
-
+/**  functions **/
 
 /*
+// 説明 //
+	引数で指定した範囲の画像情報をグローバル変数_preImgに格納する
 
- start level 2
+// 引数 //
+	srcImg	:入力画像
+	box 	:保存する画像の矩形情報（位置）
+	width 	:入力画像の幅
+	height 	:入力画像の高さ
+*/
 
- */
+void setXY(const unsigned char *image, bounding_box *obox, int width, int height)
+{
+	int x, y;
+	int subX1, subX2, subY1, subY2;
+	bounding_box box;
+	BoundingXY boxXY;
+	BoundingXY newBoxXY;
+
+	box = *obox;
+	x = box.x;
+	y = box.y;
+
+	// 前フレームの移動物体の矩形情報保存
+	boxXY.minX = box.x;
+	boxXY.maxX = box.x + box.w - 1;
+	boxXY.minY = box.y;
+	boxXY.maxY = box.y + box.h - 1;
+
+	/* 前フレームと現フレームの探索範囲での変化を調べ、移動物体位置を探索
+	上下左右それぞれの方向に走査していく
+	外側に向けて変化している位置を特定 */
+	newBoxXY = searchOutside(image, boxXY, width);
+
+	// 移動物体の幅や高さと変化したそれぞれの位置をもとに移動物体の位置を特定
+	// x方向とy方向それぞれ変化の大きい方の計算を行う
+	subX1 = abs(boxXY.minX - newBoxXY.minX);
+	subX2 = abs(boxXY.maxX - newBoxXY.maxX);
+	subY1 = abs(boxXY.minY - newBoxXY.minY);
+	subY2 = abs(boxXY.maxY - newBoxXY.maxY);
+
+	x = (subX1 > subX2) ? newBoxXY.minX : newBoxXY.maxX - box.w + 1;
+	y = (subY1 > subY2) ? newBoxXY.minY : newBoxXY.maxY - box.h + 1;
+
+	obox->x = x;
+	obox->y = y;
+}
 
 
 void setSearchBox(const bounding_box box, int width, int height)
 {
+	int maxX, maxY;
+
 	// 探索範囲の初期値設定
 	_searchBox = box;
 	_searchBox.x -= SEARCH_RANGE;
@@ -262,12 +168,14 @@ void setSearchBox(const bounding_box box, int width, int height)
 	_searchBox.x = MINTEST(_searchBox.x, 0);
 	_searchBox.y = MINTEST(_searchBox.y, 0);
 
-	if ( (_searchBox.x + _searchBox.w) > width  ) {
-		_searchBox.w -= (_searchBox.x + _searchBox.w) - width;
+	maxX = _searchBox.x + _searchBox.w;
+	if ( maxX > width  ) {
+		_searchBox.w -= maxX - width;
 	}
 
-	if ( (_searchBox.y + _searchBox.h) > height  ) {
-		_searchBox.h -= (_searchBox.y + _searchBox.h) - height;
+	maxY = _searchBox.y + _searchBox.h;
+	if ( maxY > height  ) {
+		_searchBox.h -= maxY - height;
 	}
 }
 
@@ -391,14 +299,6 @@ BoundingXY searchInside(const unsigned char *image, BoundingXY boxXY, int width,
 			if ( err > merr ) {
 				amp = sobelFilter(image, m, n, width, height);
 				if ( amp >= MAMP ) {
-					if ( _prePos[0][1] != -1 && _prePos[0][1] < n ) {
-						amp = sobelFilter(image, _prePos[0][0], _prePos[0][1], width, height);
-						if ( amp >= MAMP ) {
-							n = _prePos[0][1];
-						}
-					}
-					_prePos[0][0] = m;
-					_prePos[0][1] = n;
 					y1 = n;
 					missCount = -1;
 					break;
@@ -416,14 +316,6 @@ BoundingXY searchInside(const unsigned char *image, BoundingXY boxXY, int width,
 			if ( err > merr ) {
 				amp = sobelFilter(image, m, n, width, height);
 				if ( amp >= MAMP ) {
-					if ( _prePos[1][1] != -1 && _prePos[1][1] > n ) {
-						amp = sobelFilter(image, _prePos[1][0], _prePos[1][1], width, height);
-						if ( amp >= MAMP ) {
-							n = _prePos[1][1];
-						}
-					}
-					_prePos[1][0] = m;
-					_prePos[1][1] = n;
 					y2 = n;
 					missCount = -1;
 					break;
@@ -441,14 +333,6 @@ BoundingXY searchInside(const unsigned char *image, BoundingXY boxXY, int width,
 			if ( err > merr ) {
 				amp = sobelFilter(image, m, n, width, height);
 				if ( amp >= MAMP ) {
-					if ( _prePos[2][1] != -1 && _prePos[2][0] < m ) {
-						amp = sobelFilter(image, _prePos[2][0], _prePos[2][1], width, height);
-						if ( amp >= MAMP ) {
-							m = _prePos[2][0];
-						}
-					}
-					_prePos[2][0] = m;
-					_prePos[2][1] = n;
 					x1 = m;
 					missCount = -1;
 					break;
@@ -466,14 +350,6 @@ BoundingXY searchInside(const unsigned char *image, BoundingXY boxXY, int width,
 			if ( err > merr ) {
 				amp = sobelFilter(image, m, n, width, height);
 				if ( amp >= MAMP ) {
-					if ( _prePos[3][1] != -1 && _prePos[3][0] > m ) {
-						amp = sobelFilter(image, _prePos[3][0], _prePos[3][1], width, height);
-						if ( amp >= MAMP ) {
-							m = _prePos[3][0];
-						}
-					}
-					_prePos[3][0] = m;
-					_prePos[3][1] = n;
 					x2 = m;
 					missCount = -1;
 					break;
@@ -513,51 +389,6 @@ void setXY2(const unsigned char *image, bounding_box *obox, int width, int heigh
 	obox->w = boxXY.maxX - boxXY.minX + 1;
 	obox->h = boxXY.maxY - boxXY.minY + 1;
 }
-
-/* レベル2用 */
-void my_tracking_level2( int frameID, unsigned char *image, int width, int height )
-{
-	if ( frameID == 0 ) {
-		// 移動物体を検出するため全領域を設定
-		obox.x = 0;
-		obox.y = 0;
-		obox.w = width;
-		obox.h = height;
-
-		for ( int y = 0; y < 4; y++ ) {
-			for ( int x = 0; x < 2; x++ ) {
-				_prePos[y][x] = -1;
-			}
-		}
-
-		/* 移動物体を囲む矩形を設定 */
-		set_result( frameID, obox );
-
-		setSearchBox(obox, width, height);
-		setPreImg(image, _searchBox, width, height);
-	} else {
-		/* 前回の物体位置を基準にして現在の物体位置を探索 */
-		setXY2(image, &obox, width, height, frameID);
-		// printf("%d %d\n",obox.x, obox.y );
-
-		/* 移動物体を囲む矩形を保存 */
-		set_result( frameID, obox );
-
-		setSearchBox(obox, width, height);
-		/* 現在フレームの探索範囲を確保 */
-		setPreImg(image, _searchBox, width, height);
-	}
-}
-
-/* レベル3用 */
-void my_tracking_level3( int frameID, unsigned char *image, int width, int height )
-{
-	my_tracking_level2(frameID, image, width, height);
-}
-
-
-
-
 
 
 
